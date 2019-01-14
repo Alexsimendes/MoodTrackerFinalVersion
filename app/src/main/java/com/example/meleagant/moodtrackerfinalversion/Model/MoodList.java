ee36363b547c111a0a58adab1658cf54f4433689
MeleagantB <alexandre.simoes.mendes@outlook.fr>
2019-01-14 17:49:42 +0100

    MoodList creation in Model package

--- app/src/main/java/com/example/meleagant/moodtrackerfinalversion/Model/MoodList.java
+++ app/src/main/java/com/example/meleagant/moodtrackerfinalversion/Model/MoodList.java
@@ -3,5 +3,37 @@ package com.example.meleagant.moodtrackerfinalversion.Model;
 
 
 public class MoodList {
-    
+    private int mMood;
+    private String mComment;
+    private int mDate;
+
+    MoodList(int date, int mood, String comment) {
+        mDate = date;
+        mMood = mood;
+        mComment = comment;
+    }
+
+    public int getDate() {
+        return this.mDate;
+    }
+
+    public void setDate(int date) {
+        mDate = date;
+    }
+
+    public int getMood() {
+        return this.mMood;
+    }
+
+    void setMood(int mood) {
+        mMood = mood;
+    }
+
+    public String getComment() {
+        return this.mComment;
+    }
+
+    void setComment(String comment) {
+        mComment = comment;
+    }
 }
