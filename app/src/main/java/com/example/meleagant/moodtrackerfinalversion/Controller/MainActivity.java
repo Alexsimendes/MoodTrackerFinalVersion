057d7d7ad7c897ebdd4dfd35a40310956cd6427b
MeleagantB <alexandre.simoes.mendes@outlook.fr>
2019-01-14 15:51:36 +0100

    Main Activity XML creation, add resources

--- app/src/main/java/com/example/meleagant/moodtrackerfinalversion/Controller/MainActivity.java
+++ app/src/main/java/com/example/meleagant/moodtrackerfinalversion/Controller/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.meleagant.moodtrackerfinalversion.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.example.meleagant.moodtrackerfinalversion.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RelativeLayout mColor;
+    private ImageView mSmiley;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mColor = findViewById(R.id.activity_main_layout);
+        mSmiley = findViewById(R.id.activity_main_smiley_img);
+
     }
 }
