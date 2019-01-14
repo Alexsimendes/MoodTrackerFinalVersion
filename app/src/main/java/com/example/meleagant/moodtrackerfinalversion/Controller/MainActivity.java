27c074fe6484448cfaf9ae967c1c9a48c894b79c
MeleagantB <alexandre.simoes.mendes@outlook.fr>
2019-01-14 15:44:03 +0100

    M-V-C

--- app/src/main/java/com/example/meleagant/moodtrackerfinalversion/Controller/MainActivity.java
+++ app/src/main/java/com/example/meleagant/moodtrackerfinalversion/Controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.meleagant.moodtrackerfinalversion;
+package com.example.meleagant.moodtrackerfinalversion.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.meleagant.moodtrackerfinalversion.R;
 
 public class MainActivity extends AppCompatActivity {
 
